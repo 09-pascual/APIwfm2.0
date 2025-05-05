dafc2b0f317386d47169d728bb7195e4a2f4665b
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2025-05-05 01:14:29 -0400

    user model, serializer, and viewset fixed

--- wfmproject/urls.py
+++ wfmproject/urls.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from wfmapi.views import register
-from wfmapi.models import User
+from wfmapi.views import UserViewSet
+
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 # Register viewsets
-
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register.register_user),
     path('login', register.login_user),
-    path('users', User)
 ]
 
