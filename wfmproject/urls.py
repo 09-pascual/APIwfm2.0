c4b894a269336c03715ef1cffbd27897f6e7463d
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2025-05-05 00:31:55 -0400

    login and register work properly

--- wfmproject/urls.py
+++ wfmproject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from wfmapi.views import register
+from wfmapi.models import User
 
 router = routers.DefaultRouter(trailing_slash=False)
 # Register viewsets
@@ -10,6 +11,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register.register_user),
-    path('login', register.login_user)
+    path('login', register.login_user),
+    path('users', User)
 ]
 
