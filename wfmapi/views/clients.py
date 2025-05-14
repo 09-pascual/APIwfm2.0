61ec74dc30bf7823fb227f2233daec84f5192342
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2025-05-14 17:36:57 -0400

    created client viewset and serializer. complete the others later

---
+++ wfmapi/views/clients.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers, viewsets
+from rest_framework.permissions import IsAuthenticated
+from wfmapi.models import Client
+
+class ClientSerializer(serializers.ModelSerializer):
+    class Meta:
+        moddel = Client
+        fields = ['id', 'first_name', 'last_name', 'address', 'phone_number', 'email']
+
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]
+
